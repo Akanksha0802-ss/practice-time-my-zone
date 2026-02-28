ff7578b11a4469483f280319c218d070cfee8ec6
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-28 23:59:02 +0530

    Added notes.js

---
+++ JS/L_20/notes.js
@@ -0,0 +1,20 @@
+//  Callbacks
+// A callback function is a function that is passed as an argument to another function and executed later.
+
+
+//notes:Topic JavaScript - Browser Object Model
+// The Browser Object Model (BOM) in JavaScript helps to interact with the browser, not just the webpage. 
+
+// DOM handles the content of the page
+
+// BOM gives you control over things like the browser window, the URL, and the history.
+
+// BOM helps JavaScript work with the browser to make web pages more interactive.
+
+//Browser Object Model Types
+// 1. Window Object - The window object provides methods like alert(), confirm(), and prompt().
+// 2. Navigator Object - The navigator object provides information about the browser and the user's environment. It is often used to detect the browser type or features.
+// 3. Location Object
+// 4. Screen Object
+// 5. History Object
+// 6. Using window.resizeTo
