b61ed1d65e2174035a9011c7ce84d301906f0a94
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-03-03 12:29:06 +0530

    Added notes of L_15

---
+++ JS/L_15/notes.js
@@ -0,0 +1,21 @@
+//Non primitve data types are reference data types which kya used krteh h stack  but km heap, heap hota storage services, wo print kaha se hoti h stack se
+
+// How GEC work:
+// first step is memory allocation
+// second is code execution
+// variable var hoga toh undefined milega
+// let aur const h toh TDZ(Temporal Dead Zone milega) , jb tk ushki value initialize nhi ho rahi h tb tk
+
+//Data Types:  Two types of data types- 
+// 1. Prmitive data types like number, string, bigInt, undefined, null, boolean, this all are used stack
+// 2. non Primitive(reference) data types - this used heap, heap meh random memory allocation hota h 
+// object , array, function this all data types used heap
+
+
+
+//By Default aap koi bhi package aap install krege in your application, it comes in carrat version 
+
+// vesrion - "^5.1.0" 
+// here 5 is legacy code mtlb 5 times esh code koh scratch se likha gya h( 5th time ye code rewrite kiya gya h)
+// 1 baar minor code changes hua h( 1 baar minor code changes)
+// 0 times bug fixes kiya gya h(zero hua bug fixes)
