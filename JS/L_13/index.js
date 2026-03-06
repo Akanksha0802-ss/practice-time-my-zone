ab5f4221a7c285f4e893966308cb83debd1cf67d
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-03-07 00:58:25 +0530

    Added learning of first program in js of L_13

---
+++ JS/L_13/index.js
@@ -0,0 +1,34 @@
+//Empty file is the shortest program in javascript this is not in another programming language
+//javascript koh hm doh tarikeh se likh skteh h: ek external and internal two ways se aap likh skteh ho both are coming under script tag
+// console.log("Hello World:");
+
+// Compilation two trh keh hoteh h scripting tag meh(in Javascript) first is JIT(Just in Time ) second is AOT(Ahead of Time)
+
+
+//JIT and GEC imp ye doh smjh meh aana chaiye in js
+
+
+// console.log(a);
+
+// var a = 10;
+// var a = 20;
+// a = 40;
+// console.log(a)
+
+// {
+//     var a = 10;
+// };
+// console.log(a);
+
+
+// function test() {
+//     var a = 20;
+//     console.log(a);
+// }
+// console.log(a);
+
+
+
+
+
+
